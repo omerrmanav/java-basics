73fb1e983ed4cbddaf60aacb395ae3519fad7443
Ömer Manav <129552867+omerrmanav@users.noreply.github.com>
2023-04-05 01:11:02 +0300

    Delete aboutEnum/enum directory

--- aboutEnum/enum/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Months aylar = Months.Ağustos;
-        System.out.println(aylar + " yılın " + aylar.getMonth() + ". ayıdır.");
-    }
-}
