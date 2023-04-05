87ef7aec153a55c188ceec97a58d1ffd56c87c10
Ömer Manav <129552867+omerrmanav@users.noreply.github.com>
2023-04-05 23:55:19 +0300

    Update App.java

--- Array Lists/src/App.java
+++ Array Lists/src/App.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Sorting array
         System.out.println("Sort of Array");
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(10);
@@ -11,8 +13,31 @@ public class App {
         numbers.add(21);
         numbers.add(3);
         Collections.sort(numbers);
-        for(int i : numbers){
+        for (int i : numbers) {
             System.out.print(i + " ");
         }
+        System.out.println("\n\n");
+
+        // Sorting array by getting array length and array elements from user
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Length of Array : ");
+        int n = input.nextInt();
+        System.out.println("Enter elements of array");
+        for (int i = 0; i < n; i++) {
+            int temp = i;
+            temp = input.nextInt();
+            numbers2.add(temp);
+        }
+        System.out.println("\n-----Array-----");
+        for (int j : numbers2) {
+            System.out.print(j + " ");
+        }
+        System.out.println("\n-----Sorted Array-----");
+        Collections.sort(numbers2);
+        for (int k : numbers2) {
+            System.out.print(k + " ");
+        }
+        input.close();
     }
 }
